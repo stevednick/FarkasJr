29063d1f6b465cbbccf5d0122c5cee245dfb3828
stevednick <stephendnicholls@yahoo.co.uk>
2023-02-06 12:57:14 +0000

    Initial Commit

---
+++ FarkasJr/FarkasJrApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarkasJrApp.swift
+//  FarkasJr
+//
+//  Created by Stephen Nicholls on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FarkasJrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
